30fc0c1d43dfa5ee3f1747a14a2c6568ff807fbe
Vova Avsiievych <kovalleonid4@gmail.com>
2025-08-08 03:01:27 +0300

    Enhance drawing functionality in lesson4_1.py by implementing movement and color change features for the pen. Added methods for setting pen color to white and improved click and drag handling to restrict movement based on y-coordinate.

--- m5/lesson4_1.py
+++ m5/lesson4_1.py
@@ -5,7 +5,9 @@ screen = getscreen()
 screen.bgcolor("black")
 
 def go_xy(t, x, y):
-    ...
+    t.pu()
+    t.goto(x,y)
+    t.pd()
 
 def draw_line(t, x1, y1, x2, y2, cl="green"):
     t.color(cl)
@@ -39,23 +41,27 @@ def create_pen():
         ...
 
     def on_key_up():
-        ...
+        pen.seth(90)
+        pen.fd(10)
 
     def on_key_down():
         ...
        
     def set_red():
-        ...
+        pen.color("red")
 
     def set_green():
-        ...
+        pen.color("green")
 
     def set_blue():
-        ...
+        pen.color("blue")
+    def set_white():
+        pen.color("white")
 
     pen.set_red = set_red
     pen.set_green = set_green
     pen.set_blue = set_blue
+    pen.set_white = set_white
 
     pen.on_key_left = on_key_left
     pen.on_key_right = on_key_right
@@ -101,10 +107,15 @@ draw_rect(getpen(),-200, 200, 400, 70, "green")
 pen = create_pen()
 
 def on_screen_click(x, y):
-    ...
+    if y>120:
+        return
+    go_xy(pen,x,y)
 
 def on_drag(x, y):
-    ...
+    if y>120:
+        return
+    pen.goto(x,y)
+    
 
 btn_width_up = create_button(100, 180, "red")
 btn_width_down = create_button(100, 150, "blue")
@@ -118,7 +129,7 @@ def on_width_down(x, y):
     ...
 
 def on_clear(x, y):
-    ...
+    pen.clear()
 
 btn_clear.onclick(on_clear)
 btn_width_up.onclick(on_width_up)
@@ -139,14 +150,17 @@ def on_end_fill():
 #screen.onkey(pen.on_key_up, "")
 #screen.onkey(pen.on_key_down, "")
 
-#screen.onkey(pen.set_red, "")
-#screen.onkey(pen.set_green, "")
-#screen.onkey(pen.set_blue, "")
+screen.onkey(pen.set_red, "r")
+screen.onkey(pen.set_green, "g")
+screen.onkey(pen.set_blue, "b")
+
+screen.onkey(pen.set_white, "w")
 #screen.onkey(pen.clear, "")
 #screen.onkey(on_begin_fill, "")
 #screen.onkey(on_end_fill, "")
 
-#drag
+#dragscreen.onclick(on_screen_click)
+pen.ondrag(on_drag)
 screen.onclick(on_screen_click)
 screen.listen()
 
